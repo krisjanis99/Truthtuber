6fba5dafb5d1e06d202ecdfa2c1f138068a76447
Kris <39033244+krisjanis99@users.noreply.github.com>
2021-05-10 08:37:37 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,67 @@
+'''
+PyPower Projects
+Emotion Detection Using AI
+'''
+
+#USAGE : python test.py
+
+from keras.models import load_model
+from time import sleep
+from keras.preprocessing.image import img_to_array
+from keras.preprocessing import image
+import cv2
+import numpy as np
+
+from mss import mss
+from PIL import Image
+
+bounding_box = {'top': 0, 'left': 100, 'width': 720, 'height': 900}
+gui_output_frame = {'top': 0, 'left': 0, 'width': 960, 'height': 540}
+
+face_classifier = cv2.CascadeClassifier('./haarcascade_frontalface_default.xml')
+classifier =load_model('./Emotion_Detection.h5')
+
+class_labels = ['Angry','Happy','Neutral','Sad','Surprise']
+
+cap = mss()
+
+
+while True:
+    # Grab a single frame of video
+    frame = cap.grab(bounding_box)#cap.read()
+    labels = []
+    frame = np.array(frame)
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    #cv2.imshow('screen', frame)
+    faces = face_classifier.detectMultiScale(gray,1.3,5)
+
+    for (x,y,w,h) in faces:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+        roi_gray = gray[y:y+h,x:x+w]
+        roi_gray = cv2.resize(roi_gray,(48,48),interpolation=cv2.INTER_AREA)
+
+
+        if np.sum([roi_gray])!=0:
+            roi = roi_gray.astype('float')/255.0
+            roi = img_to_array(roi)
+            roi = np.expand_dims(roi,axis=0)
+
+        # make a prediction on the ROI, then lookup the class
+
+            preds = classifier.predict(roi)[0]
+            print("\nprediction = ",preds)
+            label=class_labels[preds.argmax()]
+            print("\nprediction max = ",preds.argmax())
+            print("\nlabel = ",label)
+            label_position = (x,y)
+            cv2.putText(frame,label,label_position,cv2.FONT_HERSHEY_SIMPLEX,2,(0,255,0),3)
+        else:
+            cv2.putText(frame,'No Face Found',(20,60),cv2.FONT_HERSHEY_SIMPLEX,2,(0,255,0),3)
+        print("\n\n")
+    cv2.imshow('Emotion Detector', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
